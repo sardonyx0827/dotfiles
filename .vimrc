7295358f150834bf63fe6bbd7e985d8c88e8efcd
sardonyx0827 <sardonyx0827@gmail.com>
2023-10-01 14:02:40 +0900

    edit wezterm:add settings about font, window, opacity

--- .vimrc
+++ .vimrc
@@ -924,7 +924,6 @@ vnoremap <leader>rem  :RExtractMethod<cr>
 
 " rust
 " Vim racer
-au FileType rust nmap gd <Plug>(rust-def)
 au FileType rust nmap gs <Plug>(rust-def-split)
 au FileType rust nmap gx <Plug>(rust-def-vertical)
 
--- .wezterm.lua
+++ .wezterm.lua
@@ -6,12 +6,15 @@ if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
--- Theme
--- config.color_scheme = 'AdventureTime'
-config.window_background_opacity = 0.85
+-- font
 config.font = wezterm.font("Ubuntu Mono", {weight="Medium", stretch="Normal", style="Normal"})
-
-
-
+-- font-size (default: 12)
+config.font_size = 12
+-- window (default: "TITLE | RESIZE")
+config.window_decorations = "RESIZE"
+-- remove tab bar
+config.hide_tab_bar_if_only_one_tab = true
+-- background_opacity (no bg image)
+config.window_background_opacity = 0.8
 
 return config
