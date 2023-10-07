4cbb0deb96fc46d4967cd9fac5b9e8c77599cce8
sardonyx0827 <sardonyx0827@gmail.com>
2023-10-07 16:39:04 +0900

    add .config/nvim config file in lua

---
+++ .config/nvim/lua/theprimeagen/init.lua
@@ -0,0 +1,39 @@
+require("theprimeagen.set")
+require("theprimeagen.remap")
+
+-- DO NOT INCLUDE THIS
+--vim.opt.rtp:append("~/personal/streamer-tools")
+-- DO NOT INCLUDE THIS
+
+local augroup = vim.api.nvim_create_augroup
+local ThePrimeagenGroup = augroup('ThePrimeagen', {})
+
+local autocmd = vim.api.nvim_create_autocmd
+local yank_group = augroup('HighlightYank', {})
+
+function R(name)
+    require("plenary.reload").reload_module(name)
+end
+
+autocmd('TextYankPost', {
+    group = yank_group,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 40,
+        })
+    end,
+})
+
+autocmd({"BufWritePre"}, {
+    group = ThePrimeagenGroup,
+    pattern = "*",
+    command = [[%s/\s\+$//e]],
+})
+
+vim.g.netrw_browse_split = 2
+vim.g.netrw_winsize = 80
+vim.g.netrw_brouse_split = 3
+vim.g.netrw_preview = 1
+vim.g.netrw_liststyle = 3
