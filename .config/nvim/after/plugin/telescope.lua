ed21ab481c32e3cbfc74f03f07c17a0a6f3139d8
sardonyx0827 <sardonyx0827@gmail.com>
2023-10-09 13:19:03 +0900

    edit nvim: update telescope, edit grep keymaps

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -2,13 +2,22 @@ require('telescope').setup({
   defaults = {
     file_ignore_patterns = { 'node_modules', 'vendor', 'dist', 'build' },
   },
+  pickers = {
+    live_grep = {
+        additional_args = function()
+            return {"--hidden"}
+        end
+    },
+  },
 })
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>f', builtin.find_files, {})
 vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
 vim.keymap.set('n', '<leader>gs', builtin.git_status, {})
+vim.keymap.set('n', '<leader>h', builtin.help_tags, {})
+
+-- using ripgrep. "sudo apt install ripgrep" or "brew install ripgrep"
+vim.keymap.set('n', '<leader>gr', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
-vim.keymap.set('n', '<leader>h', builtin.help_tags, {})
-
