bf096a8ef96ecedb5fbd111d5ff839dbbc4d7545
sardonyx0827 <sardonyx0827@gmail.com>
2023-10-10 10:36:47 +0900

    edit nvim: edit airline settings2

--- .config/nvim/after/plugin/vim-airline.lua
+++ .config/nvim/after/plugin/vim-airline.lua
@@ -18,6 +18,12 @@ let g:airline#extensions#tabline#show_tab_nr = 0
 let g:airline#extensions#tabline#show_tab_type = 1
 let g:airline#extensions#tabline#show_close_button = 0
 
+let g:airline_theme = 'powerlineish'
+let g:airline#extensions#branch#enabled = 1
+let g:airline#extensions#ale#enabled = 1
+let g:airline#extensions#tagbar#enabled = 1
+let g:airline_skip_empty_sections = 1
+
 if !exists('g:airline_symbols')
   let g:airline_symbols = {}
 endif
