1fdb7881bc4baf90f2aff57157ec177f8b93c6ea
sardonyx0827 <sardonyx0827@gmail.com>
2023-10-13 13:50:25 +0900

    add useful keymap for diffview

--- .config/nvim/after/plugin/diffview.lua
+++ .config/nvim/after/plugin/diffview.lua
@@ -1,2 +1,3 @@
 vim.keymap.set('n', '<leader>dvo', "<cmd>DiffviewOpen<cr>", { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>dvc', "<cmd>DiffviewClose<cr>", { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>dvh', "<cmd>DiffviewFileHistory<cr>", { noremap = true, silent = true })
