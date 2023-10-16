da11ebaf53fa9f51ac079e44bf749ce15b09dd23
sardonyx0827 <sardonyx0827@gmail.com>
2023-10-16 16:07:19 +0900

    add give_up function for rainbow-delimiters.

--- .config/nvim/after/plugin/rainbow-delimiters.lua
+++ .config/nvim/after/plugin/rainbow-delimiters.lua
@@ -1,10 +1,28 @@
 -- This module contains a number of default definitions
-local rainbow_delimiters = require 'rainbow-delimiters'
+
+local rainbow_delimiters = require("rainbow-delimiters")
+
+-- Loops through the tree and counts the number or errors.
+local function give_up()
+  print("give_up")
+  local parser = vim.treesitter.get_parser()
+  local errors = 0
+  parser:for_each_tree(function(tree)
+    if tree:root():has_error() then
+      errors = errors + 1
+    end
+  end)
+  if errors > 10 then
+    return nil
+  end
+  return rainbow_delimiters.strategy.global
+end
 
 vim.g.rainbow_delimiters = {
   strategy = {
     [''] = rainbow_delimiters.strategy['global'],
-    vim = rainbow_delimiters.strategy['local'],
+    --vim = rainbow_delimiters.strategy['local'],
+    vim = give_up,
   },
   query = {
     [''] = 'rainbow-delimiters',
