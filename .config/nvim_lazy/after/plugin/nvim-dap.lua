def3d96ca7c519e489ce0edd5a74fb03598fb14b
sardonyx0827 <sardonyx0827@gmail.com>
2023-11-30 14:14:03 +0900

    edit debug settings in nvim-dap and nvim-dap-ui

--- .config/nvim_lazy/after/plugin/nvim-dap.lua
+++ .config/nvim_lazy/after/plugin/nvim-dap.lua
@@ -26,13 +26,27 @@ dap.configurations = {
     }
   }
 }
+
+-- open and close dap-ui when debugging
+local dapui = require("dapui")
+dap.listeners.after.event_initialized["dapui_config"] = function()
+  dapui.open()
+end
+dap.listeners.before.event_terminated["dapui_config"] = function()
+  dapui.close()
+end
+dap.listeners.before.event_exited["dapui_config"] = function()
+  dapui.close()
+end
+
+vim.api.nvim_set_keymap('n', '<leader>bp', ':DapToggleBreakpoint<CR>', { silent = true })
+vim.api.nvim_set_keymap('n', '<leader>bc', ':lua require("dap").clear_breakpoints()<CR>', { silent = true })
+vim.api.nvim_set_keymap('n', '<F4>', ':lua require("dap").run_last()<CR>', { silent = true })
 vim.api.nvim_set_keymap('n', '<F5>', ':DapContinue<CR>', { silent = true })
 vim.api.nvim_set_keymap('n', '<F10>', ':DapStepOver<CR>', { silent = true })
 vim.api.nvim_set_keymap('n', '<F11>', ':DapStepInto<CR>', { silent = true })
 vim.api.nvim_set_keymap('n', '<F12>', ':DapStepOut<CR>', { silent = true })
-vim.api.nvim_set_keymap('n', '<leader>bp', ':DapToggleBreakpoint<CR>', { silent = true })
+
 --vim.api.nvim_set_keymap('n', '<leader>B', ':lua require("dap").set_breakpoint(nil, nil, vim.fn.input("Breakpoint condition: "))<CR>', { silent = true })
 --vim.api.nvim_set_keymap('n', '<leader>lp', ':lua require("dap").set_breakpoint(nil, nil, vim.fn.input("Log point message: "))<CR>', { silent = true })
---vim.api.nvim_set_keymap('n', '<leader>dr', ':lua require("dap").repl.open()<CR>', { silent = true })
---vim.api.nvim_set_keymap('n', '<leader>dl', ':lua require("dap").run_last()<CR>', { silent = true })
 
