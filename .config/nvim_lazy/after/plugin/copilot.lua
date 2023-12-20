0bfd93f8c61a19508db81983135aa263bb285b5f
sardonyx0827 <sardonyx0827@gmail.com>
2023-12-20 12:27:33 +0900

    add copilot panel settings

--- .config/nvim_lazy/after/plugin/copilot.lua
+++ .config/nvim_lazy/after/plugin/copilot.lua
@@ -18,4 +18,5 @@ require("copilot").setup({
   panel = { enabled = false },
 })
 require("copilot_cmp").setup()
-vim.keymap.set("n", "<C-p>", ":Copilot panel open<CR>", { silent = true })
+vim.keymap.set("n", "<leader>po", ":Copilot panel open<CR>", { silent = true })
+vim.keymap.set("n", "<leader>pa", ":Copilot panel accept<CR>", { silent = true })
