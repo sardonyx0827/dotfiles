dc15767614ad4c657796a9a9929ccf7a834d7f2d
sardonyx0827 <sardonyx0827@gmail.com>
2023-12-27 14:17:23 +0900

    add harpoon extend options

--- .config/nvim_lazy/after/plugin/harpoon.lua
+++ .config/nvim_lazy/after/plugin/harpoon.lua
@@ -4,6 +4,22 @@ local harpoon = require("harpoon")
 harpoon:setup()
 -- REQUIRED
 
-vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
+vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end, {desc = 'append harpoon mark'})
 vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+harpoon:extend({
+  UI_CREATE = function(cx)
+    vim.keymap.set("n", "<C-v>", function()
+      harpoon.ui:select_menu_item({ vsplit = true })
+    end, { buffer = cx.bufnr })
+
+    vim.keymap.set("n", "<C-h>", function()
+      harpoon.ui:select_menu_item({ split = true })
+    end, { buffer = cx.bufnr })
+
+    vim.keymap.set("n", "<C-t>", function()
+      harpoon.ui:select_menu_item({ tabedit = true })
+    end, { buffer = cx.bufnr })
+  end,
+})
 
