7828cfd0f3c1d5bee9755ddb4149963acf2cba17
sardonyx0827 <sardonyx0827@gmail.com>
2023-12-29 18:34:34 +0900

    colorscheme setting

--- .config/nvim_lazy/after/plugin/colorscheme.lua
+++ .config/nvim_lazy/after/plugin/colorscheme.lua
@@ -1,67 +1,43 @@
-local function set_bg_color_to_clear()
-  -- clear bg color
-  local color_scheme = vim.g.colors_name
-  require(color_scheme).setup({
-    transparent = true,
-    styles = {
-      sidebars = "transparent",
-      floats = "transparent",
-    },
-  })
-  --vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-  --vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-  vim.cmd.colorscheme(color_scheme)
-end
+vim.keymap.set("n", "<M-0>", ":colorscheme ", { noremap = true })
 
-local function set_bg_color_to_default()
-  -- set bg color to default
-  local color_scheme = vim.g.colors_name
-  require(color_scheme).setup({
-    transparent = false,
-    styles = {
-      sidebars = "bg",
-      floats = "bg",
-    },
-  })
-  --vim.cmd("colorscheme " .. color_scheme)
-  vim.cmd.colorscheme(color_scheme)
-end
-
-local transparent = false
-local function toggle_transparent()
-  if transparent then
-    set_bg_color_to_default()
-    transparent = false
-  else
-    set_bg_color_to_clear()
-    transparent = true
-  end
-end
---vim.keymap.set("n", "<M-1>", ":colorscheme vscode<CR>", { noremap = true })
---vim.keymap.set("n", "<M-2>", ":colorscheme tokyonight-moon<CR>", { noremap = true })
---vim.keymap.set("n", "<M-3>", ":colorscheme onedark<CR>", { noremap = true })
---vim.keymap.set("n", "<M-4>", ":colorscheme tokyonight-night<CR>", { noremap = true })
---vim.keymap.set("n", "<M-5>", ":colorscheme rose-pine-moon<CR>", { noremap = true })
---vim.keymap.set("n", "<M-6>", ":colorscheme rose-pine-main<CR>", { noremap = true })
---vim.keymap.set("n", "<M-7>", ":colorscheme lunaperche<CR>", { noremap = true })
---vim.keymap.set("n", "<M-8>", ":colorscheme slate<CR>", { noremap = true })
---vim.keymap.set("n", "<M-9>", ":colorscheme default<CR>", { noremap = true })
-vim.keymap.set("n", "<M-0>", toggle_transparent, { noremap = true, silent = true })
-
-local function set_random_color_scheme()
-  local color_schemes = {
-    "vscode",
-    "onedark",
-    "rose-pine-main",
-    "tokyonight-night",
-  }
+local function set_random_color_scheme(color_schemes)
   local color_scheme = color_schemes[math.random(#color_schemes)]
   vim.cmd("colorscheme " .. color_scheme)
   print("color scheme: " .. color_scheme)
 end
 --set_random_color_scheme()
+
+local function set_color_scheme_from_tmux_pane(color_scheme)
+  vim.cmd("colorscheme " .. color_scheme)
+  print("color scheme: " .. color_scheme)
+end
+local function set_color_scheme()
+  local tmux_pane_id = vim.fn.system("tmux run \"echo '#{pane_id}'\"")
+  tmux_pane_id = string.gsub(tmux_pane_id, "%%", "")
+  -- my recommended color schemes
+  local color_schemes = {
+    "rose-pine-main",
+    "vscode",
+    "onedark",
+    "tokyonight-night",
+    "slate",
+  }
+  -- tmux_pane_id is number?
+  if not tonumber(tmux_pane_id) then
+    set_random_color_scheme(color_schemes)
+  else
+    -- tmux pane id(+1) is bigger than color_schemes length?
+    if tmux_pane_id+1 > #color_schemes then
+      set_random_color_scheme(color_schemes)
+    else
+      set_color_scheme_from_tmux_pane(color_schemes[tmux_pane_id+1])
+    end
+  end
+end
+--set_color_scheme()
 -- default color scheme
---vim.cmd("colorscheme vscode")
-vim.cmd("colorscheme rose-pine-main")
+--vim.cmd("colorscheme rose-pine-main")
+vim.cmd("colorscheme vscode")
 --vim.cmd("colorscheme onedark")
 --vim.cmd("colorscheme tokyonight-night")
+--vim.cmd("colorscheme slate")
