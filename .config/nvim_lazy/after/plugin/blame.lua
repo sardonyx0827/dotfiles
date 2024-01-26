0664dbe8093c7d4d85858f53868a306bb8a71d18
sardonyx0827 <sardonyx0827@gmail.com>
2024-01-27 01:07:13 +0900

    edit blame setting on nvim

--- .config/nvim_lazy/after/plugin/blame.lua
+++ .config/nvim_lazy/after/plugin/blame.lua
@@ -1,1 +1,1 @@
-vim.keymap.set("n", "<leader>gb", "<cmd>ToggleBlame virtual<cr>", { noremap = true, silent = true, desc = "Toggle Blame - toggle git comments on line." })
+vim.keymap.set("n", "<leader>gb", "<cmd>ToggleBlame window<cr>", { noremap = true, silent = true, desc = "Toggle Blame - toggle git comments on line." })
