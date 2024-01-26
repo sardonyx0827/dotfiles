720a590eacb10b7ab94dc4100123cff179fa16d0
sardonyx0827 <sardonyx0827@gmail.com>
2024-01-26 13:47:48 +0900

    move config about select code block remap to copilot.lua

--- .config/nvim_lazy/after/plugin/copilot.lua
+++ .config/nvim_lazy/after/plugin/copilot.lua
@@ -37,8 +37,8 @@ require("copilot_cmp").setup()
 vim.keymap.set("n", "<c-p>", ":Copilot panel<CR>", { silent = true })
 vim.keymap.set("i", "<c-l>", "<ESC>:Copilot panel<CR>", { silent = true })
 
---import sys
---sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+-- Copilot Chat
 vim.keymap.set("n", "<leader>cc", "ggVGy:vertical rightbelow new<CR>:setlocal filetype=markdown<CR>:CopilotChat ", { desc = "Copilot Chat - select all" })
 vim.keymap.set("v", "<leader>cc", "y:vertical rightbelow new<CR>:setlocal filetype=markdown<CR>:CopilotChat ", { desc = "Copilot Chat - selected" })
 vim.keymap.set("n", "<leader>co", ":CopilotChat ", { desc = "Copilot Chat - ongoing" })
@@ -55,7 +55,7 @@ vim.keymap.set("v", "<leader>cj", "y:vertical rightbelow new<CR>:setlocal filety
 vim.keymap.set("n", "<leader>cs", "{V}y:vertical rightbelow new<CR>:setlocal filetype=markdown<CR>:CopilotChat ", { desc = "Copilot Chat - yank surround" })
 vim.keymap.set("n", "<leader>cl", "50kV100j50ky:vertical rightbelow new<CR>:setlocal filetype=markdown<CR>:CopilotChat ", { desc = "Copilot Chat - yank 100lines" })
 
--- jump to next error/warn and fix with Copilot
+-- jump to next error/warn and fix with Copilot Chat
 local function quick_fix_next_error_with_ai()
   if vim.diagnostic.get(0, {severity = vim.diagnostic.severity.ERROR})[1] == nil then
     print("no error")
@@ -102,3 +102,153 @@ local function quick_fix_next_error_with_ai()
   vim.cmd("CopilotChat ".. "error message : " .. diagnostic_message .. " | current line text : " .. lines_text .. " | your job : how to fix it?")
 end
 vim.keymap.set("n", "<leader>qf", quick_fix_next_error_with_ai, {desc="Jump to Next Error and fix with Copilot"})
+
+
+-- select codeblock text
+local function move_cursor_to_above_codeblock()
+  local current_line_number = vim.api.nvim_win_get_cursor(0)[1]
+  local start_line = current_line_number
+  -- search above
+  for i = current_line_number, 0, -1 do
+    local _line = vim.fn.getline(i)
+    if string.match(_line, "^```") then
+      start_line = i - 1
+      break
+    end
+    if i <= 1 then
+      start_line = 1
+      break
+    end
+  end
+  vim.api.nvim_win_set_cursor(0, {start_line, 0})
+end
+
+local function select_codeblock_text(cursor_position)
+  local current_line_number = vim.api.nvim_win_get_cursor(0)[1]
+  local start_line = current_line_number
+  local end_line = current_line_number
+  local max_line = vim.api.nvim_buf_line_count(0)
+  -- search above
+  for i = current_line_number, 0, -1 do
+    local _line = vim.fn.getline(i)
+    if string.match(_line, "^```") then
+      start_line = i + 1
+      break
+    end
+    if i == 1 then
+      start_line = 1
+      break
+    end
+  end
+  -- search below
+  for i = current_line_number, max_line do
+    local _line = vim.fn.getline(i)
+    if string.match(_line, "^```") then
+      end_line = i - 1
+      break
+    end
+  end
+  if start_line <= 1 then
+    print("no codeblock text")
+    -- restore cursor position
+    vim.api.nvim_win_set_cursor(0, {cursor_position, 0})
+  else
+    -- select lines
+    vim.api.nvim_win_set_cursor(0, {start_line, 0})
+    vim.cmd("normal! V")
+    vim.api.nvim_win_set_cursor(0, {end_line, 0})
+  end
+end
+
+local function select_last_codeblock_text()
+  -- save cursor position
+  local cursor_position = vim.api.nvim_win_get_cursor(0)[1]
+  vim.cmd("normal! G")
+  move_cursor_to_above_codeblock()
+  select_codeblock_text(cursor_position)
+end
+vim.keymap.set("n", "<leader>vmm", select_last_codeblock_text, {desc = "select codeblock text (last)", noremap = true})
+
+local function save_yanked_text(path, reg)
+  local text = vim.fn.getreg(reg)
+  if text == nil or text == "" then
+    print("no text in register")
+    return false
+  end
+  local file = io.open(path, "w")
+  if file == nil then
+    print("cannot open file")
+    return false
+  end
+  file:write(text)
+  file:close()
+  return true
+end
+
+local function get_filetype_from_codeblock()
+  vim.cmd("normal! G")
+  move_cursor_to_above_codeblock()
+  local current_line_number = vim.api.nvim_win_get_cursor(0)[1]
+  local block_line = current_line_number
+  for i = current_line_number, 0, -1 do
+    local _line = vim.fn.getline(i)
+    if string.match(_line, "^```") then
+      block_line = i
+      break
+    end
+  end
+  vim.api.nvim_win_set_cursor(0, {block_line, 0})
+
+  local filetype = vim.api.nvim_get_current_line()
+  -- extract filetype from codeblock
+  filetype = filetype:match("^```(%w+)")
+  return filetype
+end
+
+local function diff_codeblock_text()
+  local cursor_position = vim.api.nvim_win_get_cursor(0)[1]
+  vim.cmd("normal! G")
+  move_cursor_to_above_codeblock()
+  local current_line_number = vim.api.nvim_win_get_cursor(0)[1]
+  local start_line = current_line_number
+  for i = current_line_number, 0, -1 do
+    local _line = vim.fn.getline(i)
+    if string.match(_line, "^```") then
+      start_line = i + 1
+      break
+    end
+    if i == 1 then
+      start_line = 1
+      break
+    end
+  end
+  if start_line <= 1 then
+    print("no codeblock in this buffer")
+    -- restore cursor position
+    vim.api.nvim_win_set_cursor(0, {cursor_position, 0})
+    return
+  end
+  local target_text = "/tmp/_target_text"
+  local copilot_text = "/tmp/_copilot_suggestion"
+  local function save_and_check(path, register)
+    local result = save_yanked_text(path, register)
+    if not result then
+      error("Failed to save yanked text to " .. path)
+    end
+  end
+  local function diff_texts(target_text, copilot_text, filetype)
+    -- open copilot_text text to new tab
+    vim.cmd("tabnew " .. copilot_text)
+    vim.cmd("setlocal filetype=" .. filetype)
+    vim.cmd("vertical diffsplit " .. target_text)
+    vim.cmd("setlocal filetype=" .. filetype)
+  end
+  save_and_check(target_text, '"')
+  local filetype = get_filetype_from_codeblock()
+  select_last_codeblock_text()
+  vim.cmd('normal! y')
+  save_and_check(copilot_text, '"')
+  diff_texts(target_text, copilot_text, filetype)
+end
+vim.keymap.set("n", "<leader>vmd", diff_codeblock_text, {desc = "diff codeblock text", noremap = true})
+vim.keymap.set("n", "<leader>vmc", ":tabclose<CR>", {desc = "close diff tab", noremap = true})
