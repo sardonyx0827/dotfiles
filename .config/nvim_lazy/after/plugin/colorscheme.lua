db8ea20a455976c091d98522e95c6c639ea8b0c5
sardonyx0827 <sardonyx0827@gmail.com>
2024-02-02 16:30:05 +0900

    change color theme every code editors

--- .config/nvim_lazy/after/plugin/colorscheme.lua
+++ .config/nvim_lazy/after/plugin/colorscheme.lua
@@ -43,12 +43,13 @@ end
 --set_color_scheme()
 
 -- default color scheme
-vim.cmd("colorscheme rose-pine-main")
---vim.cmd("colorscheme vscode")
+--vim.cmd("colorscheme rose-pine-main")
+vim.cmd("colorscheme vscode")
 --vim.cmd("colorscheme tokyonight-night")
 --vim.cmd("colorscheme slate")
 --vim.cmd("colorscheme onedark")
 
+vim.g.tokyonight_colors = { border = "orange" }
 -- clear bg color
 --vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 --vim.api.nvim_set_hl(0, "NormalFlort", { bg = "none" })
