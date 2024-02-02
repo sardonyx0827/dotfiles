c6c629f480e6ddc1693ccd0f37f8ad3c7e0c8a23
sardonyx0827 <sardonyx0827@gmail.com>
2024-02-02 16:46:16 +0900

    change color theme every code editors

--- .config/nvim_lazy/lua/setup/lazy.lua
+++ .config/nvim_lazy/lua/setup/lazy.lua
@@ -31,9 +31,15 @@ local plugins = {
       require("tokyonight").setup({
         transparent = true,
         styles = {
+          -- Background styles. Can be "dark", "transparent" or "normal"
           sidebars = "transparent",
-          floats = "transparent",
+          --floats = "transparent",
+          --sidebars = "dark", -- style for sidebars, see below
+          floats = "dark", -- style for floating windows
         },
+        on_colors = function(colors)
+          colors.border = "#565f89"
+        end
       })
     end
   },
