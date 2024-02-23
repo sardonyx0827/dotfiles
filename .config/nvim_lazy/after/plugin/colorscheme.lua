c6d9f06669ae329c638dfd7628b26c8f5e3fd04d
sardonyx0827 <sardonyx0827@gmail.com>
2024-02-23 16:18:24 +0900

    change colorscheme for nvim

--- .config/nvim_lazy/after/plugin/colorscheme.lua
+++ .config/nvim_lazy/after/plugin/colorscheme.lua
@@ -44,13 +44,15 @@ end
 --set_color_scheme()
 
 -- default color scheme
-vim.cmd("colorscheme rose-pine-main")
---vim.cmd("colorscheme vscode")
+--vim.cmd("colorscheme rose-pine-main")
+vim.cmd("colorscheme vscode")
 --vim.cmd("colorscheme tokyonight-night")
 --vim.cmd("colorscheme slate")
 --vim.cmd("colorscheme onedark")
 
 -- clear bg color
---vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
---vim.api.nvim_set_hl(0, "NormalFlort", { bg = "none" })
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
 
