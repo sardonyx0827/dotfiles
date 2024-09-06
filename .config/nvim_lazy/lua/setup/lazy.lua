2c15c7673e718c0aa0b7388023d84a900518a7c1
sardonyx0827 <sardonyx0827@gmail.com>
2024-09-07 00:56:37 +0900

    gitcommit chore: comment out unused cmp-path plugin
    Commented out the 'hrsh7th/cmp-path' plugin in the Neovim lazy setup
    file. This plugin is currently not in use and can be re-enabled if
    needed in the future.

--- .config/nvim_lazy/lua/setup/lazy.lua
+++ .config/nvim_lazy/lua/setup/lazy.lua
@@ -290,7 +290,7 @@ local plugins = {
     dependencies = {
       { 'hrsh7th/cmp-nvim-lsp' },
       { 'hrsh7th/cmp-buffer' },
-      { 'hrsh7th/cmp-path' },
+      -- { 'hrsh7th/cmp-path' },
       { 'hrsh7th/cmp-cmdline' },
       { 'saadparwaiz1/cmp_luasnip' },
       { 'hrsh7th/cmp-nvim-lua' },
