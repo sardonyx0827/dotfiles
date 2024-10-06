b209209395649c01606c9808f96741dededae6ac
sardonyx0827 <sardonyx0827@gmail.com>
2024-10-06 19:10:06 +0900

    gitcommit feat(nvim): add keybindings for Trouble plugin
    Added keybindings for the Trouble plugin to enhance navigation and
    diagnostics management. The new keybindings include:
    - <leader>xX: Workspace Diagnostics (Trouble)
    - <leader>xx: Buffer Diagnostics (Trouble)
    - <leader>xs: Symbols (Trouble)
    - <leader>xl: LSP Definitions / references / ... (Trouble)
    - <leader>xL: Location List (Trouble)
    - <leader>xQ: Quickfix List (Trouble)

--- .config/nvim_lazy/lua/setup/lazy.lua
+++ .config/nvim_lazy/lua/setup/lazy.lua
@@ -238,6 +238,38 @@ local plugins = {
     "folke/trouble.nvim",
     lazy = true,
     cmd = { "TroubleToggle", "Trouble", "TroubleRefresh" },
+    keys = {
+      {
+        "<leader>xX",
+        "<cmd>Trouble diagnostics toggle<cr>",
+        desc = "Workspace Diagnostics (Trouble)",
+      },
+      {
+        "<leader>xx",
+        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+        desc = "Buffer Diagnostics (Trouble)",
+      },
+      {
+        "<leader>xs",
+        "<cmd>Trouble symbols toggle focus=false<cr>",
+        desc = "Symbols (Trouble)",
+      },
+      {
+        "<leader>xl",
+        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+        desc = "LSP Definitions / references / ... (Trouble)",
+      },
+      {
+        "<leader>xL",
+        "<cmd>Trouble loclist toggle<cr>",
+        desc = "Location List (Trouble)",
+      },
+      {
+        "<leader>xQ",
+        "<cmd>Trouble qflist toggle<cr>",
+        desc = "Quickfix List (Trouble)",
+      },
+    },
     config = function()
       require("trouble").setup {
         -- icons = false,
