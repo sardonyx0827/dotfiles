748d4be233b31a277a1961cb0cd73dec1975cef0
sardonyx0827 <sardonyx0827@gmail.com>
2025-04-20 23:38:33 +0900

    feat(mcphub): enhance server configurations and add tools
    - Added new native MCP server configurations for Neovim.
    - Updated `mcpServers` with new entries like `sequential-thinking`
      and `playwright`.
    - Enhanced `fetch` server with disabled resources and templates.
    - Improved `Zapier MCP` and `browser-tools-mcp` configurations.
    
    refactor(nvim): simplify Avante and LSP plugin setups
    
    - Removed unused Avante setup code.
    - Cleaned up null-ls setup in LSP configuration.

--- .config/nvim_lazy/after/plugin/lsp.lua
+++ .config/nvim_lazy/after/plugin/lsp.lua
@@ -27,19 +27,6 @@ require('mason-lspconfig').setup({
   }
 })
 
--- setup null-ls for formatting and linting
--- local mason_package = require("mason-core.package")
--- local mason_registry = require("mason-registry")
--- local null_sources = {}
--- for _, package in ipairs(mason_registry.get_installed_packages()) do
---   local package_categories = package.spec.categories[1]
---   if package_categories == mason_package.Cat.Formatter then
---     table.insert(null_sources, null_ls.builtins.formatting[package.name])
---   end
---   if package_categories == mason_package.Cat.Linter then
---     table.insert(null_sources, null_ls.builtins.diagnostics[package.name])
---   end
--- end
 null_ls.setup({
   -- sources = null_sources,
 })
