c18ad8b8f7cb69135fcf4e4e758f903b1e476721
sardonyx0827 <sardonyx0827@gmail.com>
2025-04-30 19:56:37 +0900

    feat(wezterm): comment out font and window decoration settings
    Commented out the font and window decoration settings in the wezterm
    configuration file to allow default behavior. This change ensures
    greater flexibility and compatibility with various environments.
    
    BREAKING CHANGE: Font and window decoration settings are no longer
    explicitly set, which may alter the appearance of the terminal.

--- .wezterm.lua
+++ .wezterm.lua
@@ -10,11 +10,11 @@ config.use_ime = true
 -- Acceptable values are SteadyBlock, BlinkingBlock, SteadyUnderline, BlinkingUnderline, SteadyBar, and BlinkingBar.
 config.default_cursor_style = 'BlinkingBlock'
 -- font
-config.font = wezterm.font("Ubuntu Mono", {weight="Medium", stretch="Normal", style="Normal"})
+--config.font = wezterm.font("Ubuntu Mono", {weight="Medium", stretch="Normal", style="Normal"})
 -- font-size (default: 12)
 config.font_size = 12
 -- window (default: "TITLE | RESIZE")
-config.window_decorations = "RESIZE"
+--config.window_decorations = "TITLE"
 -- remove tab bar
 config.hide_tab_bar_if_only_one_tab = true
 -- background_opacity (no bg image)
