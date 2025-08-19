9f081a9fbb9e8e5bba4384791e5f7d947a4e79b6
sardonyx0827 <sardonyx0827@gmail.com>
2025-08-19 12:27:49 +0900

    feat(remap): clean up keymaps and add vimgrep shortcut
    Remove unused visual mode move and count up/down keymaps. Add keymaps for
    rename and vimgrep, and organize window and directory navigation shortcuts.
    Eliminate duplicates to improve readability and usability.

--- .config/nvim_lazy/lua/setup/remap.lua
+++ .config/nvim_lazy/lua/setup/remap.lua
@@ -7,22 +7,18 @@ vim.keymap.set("n", "<leader>p", ":Ex<CR>", {desc = "open file explorer"})
 vim.keymap.set("v", "<", "<gv")
 -- In visual mode, shift text right and reselect
 vim.keymap.set("v", ">", ">gv")
--- In visual mode, move selected lines down and reselect
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
--- In visual mode, move selected lines up and reselect
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 -- In normal mode, join lines and return to initial position
 vim.keymap.set("n", "J", "mzJ`z")
 -- In normal mode, scroll down half a page and center
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 -- In normal mode, scroll up half a page and center
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- search
 -- In normal mode, find next match and center
 vim.keymap.set("n", "n", "nzzzv")
 -- In normal mode, find previous match and center
 vim.keymap.set("n", "N", "Nzzzv")
--- In normal mode, rename text in this file
-vim.keymap.set("n", "<leader>rn", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], {desc = "rename text in this file"})
 -- In normal mode, clear search highlighting
 vim.keymap.set("n", "<leader><Space>", function()
   vim.cmd("noh")
@@ -31,12 +27,15 @@ end)
 vim.keymap.set("n", "<C-s>", function()
   vim.cmd("w")
 end)
--- count up/down. 'C-a' is already used in Tmux(prefix)
-vim.keymap.set("v", "<C-k>", "<C-a>gv")
-vim.keymap.set("v", "<C-j>", "<C-x>gv")
-vim.keymap.set("n", "<C-k>", "<C-a>")
-vim.keymap.set("n", "<C-j>", "<C-x>")
--- resize current window
+-- In normal mode, rename text in this file
+vim.keymap.set("n", "<leader>rn", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], {desc = "rename text in this file"})
+-- vimgrep and open quickfix window
+vim.keymap.set("n", "<leader>vg", function()
+  vim.cmd("vimgrep /" .. vim.fn.input("Grep For > ") .. "/ **/*")
+  vim.cmd("copen")
+end, {desc = "vimgrep and open quickfix window"})
+
+-- window
 -- make the window biger vertically
 vim.keymap.set('n', '<C-up>', '1<C-w>+', { noremap = true, silent = true})
 -- make the window smaller vertically
@@ -51,7 +50,6 @@ vim.keymap.set("n", "<C-h>", ":bprev<CR>", { noremap = true, silent = true})
 -- change directory to current file
 vim.keymap.set("n", "<leader>cd", ":cd %:h<CR>", {desc = "change directory to current file"})
 vim.keymap.set("n", "<leader>cu", ":cd ..<CR>", {desc = "change up directory"})
-
 -- move cursor in insert mode
 vim.keymap.set("i", "<C-j>", "<C-o>gj")
 vim.keymap.set("i", "<C-k>", "<C-o>gk")
@@ -61,8 +59,3 @@ vim.keymap.set("i", "<C-l>", "<C-o>l")
 -- keep last yanked when pasting
 vim.keymap.set("v", "p", "\"_dP", {desc = "keep last yanked when pasting"})
 
--- vimgrep and open quickfix window
-vim.keymap.set("n", "<leader>vg", function()
-  vim.cmd("vimgrep /" .. vim.fn.input("Grep For > ") .. "/ **/*")
-  vim.cmd("copen")
-end, {desc = "vimgrep and open quickfix window"})
