dc7b45c2261f4cd1ed976aeeb933a62525853348
sardonyx0827 <sardonyx0827@gmail.com>
2025-11-09 20:34:15 +0900

    refactor(nvim): simplify harpoon keymaps with loop
    Replace repetitive vim.keymap.set calls (1-9) with a for loop to improve
    maintainability and reduce code duplication.

--- .config/nvim_lazy/lua/setup/plugins/utilities/harpoon.lua
+++ .config/nvim_lazy/lua/setup/plugins/utilities/harpoon.lua
@@ -36,14 +36,8 @@ return {
     })
     vim.keymap.set("n", "<leader>ha", function() harpoon:list():append() end, { desc = 'harpoon append' })
     vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-    vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
-    vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
-    vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end)
-    vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end)
-    vim.keymap.set("n", "<leader>5", function() harpoon:list():select(5) end)
-    vim.keymap.set("n", "<leader>6", function() harpoon:list():select(6) end)
-    vim.keymap.set("n", "<leader>7", function() harpoon:list():select(7) end)
-    vim.keymap.set("n", "<leader>8", function() harpoon:list():select(8) end)
-    vim.keymap.set("n", "<leader>9", function() harpoon:list():select(9) end)
+    for i = 1, 9 do
+      vim.keymap.set("n", "<leader>" .. i, function() harpoon:list():select(i) end)
+    end
   end,
 }
