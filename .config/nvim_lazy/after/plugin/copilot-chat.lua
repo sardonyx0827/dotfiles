7b06ac870a50357313b3e988e72b01cfd748493c
sardonyx0827 <sardonyx0827@gmail.com>
2025-11-12 20:40:30 +0900

    feat(copilot-chat): add CopilotChat.nvim plugin setup
    Added CopilotChat.nvim plugin configuration with GPT-4.1 model and keymap
    for commit message generation. Integrated required dependency plenary.nvim.

---
+++ .config/nvim_lazy/after/plugin/copilot-chat.lua
@@ -0,0 +1,2 @@
+--- @diagnostic disable: undefined-global
+vim.keymap.set("n", "<leader>cm", ":CopilotChatCommit<CR>", { desc = "Copilot Chat - Write commit message for the change with commitizen convention" })
---
+++ .config/nvim_lazy/lua/setup/plugins/ai/copilot-chat.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    dependencies = {
+      { "nvim-lua/plenary.nvim", branch = "master" },
+    },
+    build = "make tiktoken",
+    opts = {
+      -- See Configuration section for options
+      model = 'gpt-4.1',
+
+    },
+  },
+}
