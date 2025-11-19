2956f32540ec069c9fdd24470452abc5be5472ef
sardonyx0827 <sardonyx0827@gmail.com>
2025-11-19 12:36:51 +0900

    feat(keybindings): add <leader>j mappings
    Introduce a <leader>j prefix for hopping commands in Neovim and VS
    Code settings. Add mappings:
    - <leader>jj -> HopWord
    - <leader>js -> HopPattern
    - <leader>jc -> HopChar1
    
    Update VS Code keyboard remaps to forward '<leader> j s' and
    '<leader> j c' to the existing easy-motion behavior.

--- .config/nvim_lazy/lua/setup/plugins/utilities/hop.lua
+++ .config/nvim_lazy/lua/setup/plugins/utilities/hop.lua
@@ -4,7 +4,8 @@ return {
   version = 'v2',
   config = function()
     vim.keymap.set("n", '<leader>jj', ':HopWord<CR>', { remap = true, silent = true, desc = "hop - move to any word" })
-    vim.keymap.set("n", '<leader>ss', ':HopPattern<CR>', { remap = true, silent = true, desc = "hop - move to pattern" })
+    vim.keymap.set("n", '<leader>js', ':HopPattern<CR>', { remap = true, silent = true, desc = "hop - move to searched pattern" })
+    vim.keymap.set("n", '<leader>jc', ':HopChar1<CR>', { remap = true, silent = true, desc = "hop - move to two character" })
     require("hop").setup({
     })
   end
