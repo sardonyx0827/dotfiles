8553b8dd1066615d3795d53060db694414b7ac84
sardonyx0827 <sardonyx0827@gmail.com>
2025-11-21 11:15:22 +0900

    feat(snacks): add keymaps and tweak defaults
    Add key mappings for snacks.nvim to expose search/command history,
    LSP symbols/workspace symbols, scratch toggle and word-jumps (]] / [[).
    Also comment out several module flags to rely on defaults or disable
    them as desired.
    
    Disable undefined-global diagnostics for the file to avoid lsp warnings.

--- .config/nvim_lazy/lua/setup/plugins/visual/snacks.lua
+++ .config/nvim_lazy/lua/setup/plugins/visual/snacks.lua
@@ -1,3 +1,4 @@
+--- @diagnostic disable: undefined-global
 --- @diagnostic disable: undefined-doc-name
 return {
   "folke/snacks.nvim",
@@ -8,17 +9,26 @@ return {
     -- your configuration comes here
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
-    bigfile = { enabled = true },
+    -- bigfile = { enabled = true },
     -- dashboard = { enabled = true },
-    explorer = { enabled = false },
+    -- explorer = { enabled = true },
     -- indent = { enabled = true },
-    input = { enabled = true },
+    -- input = { enabled = true },
     picker = { enabled = true },
-    notifier = { enabled = true },
-    quickfile = { enabled = true },
-    scope = { enabled = true },
+    -- notifier = { enabled = true },
+    -- quickfile = { enabled = true },
+    -- scope = { enabled = true },
     scroll = { enabled = true },
-    statuscolumn = { enabled = true },
+    -- statuscolumn = { enabled = true },
     words = { enabled = true },
   },
+  keys = {
+    { '<leader>h/', function() Snacks.picker.search_history() end, desc = "Search History" },
+    { "<leader>h:", function() Snacks.picker.command_history() end, desc = "Command History" },
+    { "<leader>o",  function() Snacks.picker.lsp_symbols() end, desc = "LSP Symbols" },
+    { "<leader>O", function() Snacks.picker.lsp_workspace_symbols() end, desc = "LSP Workspace Symbols" },
+    { "<leader>.",  function() Snacks.scratch() end, desc = "Toggle Scratch Buffer" },
+    { "]]",         function() Snacks.words.jump(vim.v.count1) end, desc = "Next Reference", mode = { "n", "t" } },
+    { "[[",         function() Snacks.words.jump(-vim.v.count1) end, desc = "Prev Reference", mode = { "n", "t" } },
+  }
 }
