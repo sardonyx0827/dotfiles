6b620fd7d42f2e113cd5176b369739ee92894168
sardonyx0827 <sardonyx0827@gmail.com>
2026-03-04 15:39:00 +0900

    fix: disable synchronize-panes temporarily in tmux broadcast script
    - Save current synchronize-panes state before broadcasting
    - Disable synchronize-panes temporarily to ensure command is sent only to non-nvim panes
    - Restore previous state after broadcasting completes
    - Fix indentation to use tabs consistently

--- tmux_send_to_all_except_nvim.sh
+++ tmux_send_to_all_except_nvim.sh
@@ -3,11 +3,24 @@
 # 送信したいコマンド
 COMMAND="$*"
 
+# synchronize-panes の現在の状態を取得
+SYNC_STATE=$(tmux show-window-option -v synchronize-panes 2>/dev/null)
+
+# sync がオンなら一時的にオフにする
+if [[ "$SYNC_STATE" == "on" ]]; then
+	tmux set-window-option synchronize-panes off
+fi
+
 # 現在のウィンドウのすべてのペインをリスト
 tmux list-panes -F '#{pane_id} #{pane_current_command}' | while read -r PANE_ID COMMAND_NAME; do
-    # 実行中のコマンドが 'nvim' でない場合
-    if [[ "$COMMAND_NAME" != "nvim" ]]; then
-        # send-keys でコマンドを送信
-        tmux send-keys -t "$PANE_ID" "$COMMAND"
-    fi
+	# 実行中のコマンドが 'nvim' でない場合
+	if [[ "$COMMAND_NAME" != "nvim" ]]; then
+		# send-keys でコマンドを送信
+		tmux send-keys -t "$PANE_ID" "$COMMAND"
+	fi
 done
+
+# sync がオンだった場合は元に戻す
+if [[ "$SYNC_STATE" == "on" ]]; then
+	tmux set-window-option synchronize-panes on
+fi
